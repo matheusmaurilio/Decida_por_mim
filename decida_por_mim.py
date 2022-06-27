1757dac357fc72019bc2f6ae81d09f8c880601a6
Matheus Maurilio <mathmaurilio@gmail.com>
2022-06-27 16:19:45 -0300

    Versão 1.0

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -1,6 +1,7 @@
 #Esse programa permite que o usuário faça uma pergunta e escreve uma resposta para ela
 
 import random
+import PySimpleGUI as PSG
 
 class DecidaPorMim:
     def __init__(self):
@@ -11,8 +12,21 @@ class DecidaPorMim:
             'Você não tem nada melhor para fazer? '
         ]
     def Iniciar(self):
-        input('Faça sua pergunta: ')
-        print(random.choice(self.respostas))
+        layout = [ 
+            [PSG.Text('Faça sua pergunta:')],
+            [PSG.Input()],
+            [PSG.Output((20,10))],
+            [PSG.Button('Decida por mim')]
+        ]
+        self.janela = PSG.Window('Decida por mim', layout)
+        while True:
+            self.eventos, self.valores = self.janela.Read() 
 
+            if self.eventos == 'Decida por mim':
+                print(random.choice(self.respostas))
+            if self.eventos == PSG.WIN_CLOSED:    
+                break
+            
+            
 decida_por_mim = DecidaPorMim()
 decida_por_mim.Iniciar()
